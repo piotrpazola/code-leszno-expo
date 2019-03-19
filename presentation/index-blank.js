d4022e7da32b766b3483e9b1ce2e0357c36e5d45
Piotr Pazoła <piotr@webtrip.pl>
2019-03-20 00:14:19 +0100

    Spectacle Boilerplate

---
+++ presentation/index-blank.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Presentation extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Spectacle Tutorial</h1>
+      </div>
+    );
+  }
+}
